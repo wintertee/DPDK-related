77f1146547141bfd8e1f0aec874309ccb5707d2b
冬茶 <27325188+wintertee@users.noreply.github.com>
2021-03-22 22:11:07 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,9 +1,5 @@
 # update dev to master
-
-git pull origin dev
-git pull origin master
-
-git checkout dev
-git rebase master
+git checkout master
+git rebase dev
 git push origin dev --force
 git push origin master --force
